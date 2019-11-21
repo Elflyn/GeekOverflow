7238ef68c08197ea4f749eba540d648a003e0636
Evelyn Zhang <ruz164@ucsd.edu>
2019-11-20 18:15:49 -0800

    add a router for the app

---
+++ TritonDeal/RouterComponent.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
+import {Scene, Router, Stack, Actions} from 'react-native-router-flux';
+import LoginPage from './src/views/LoginPage';
+import SignupPage from './src/views/SignupPage';
+import {Icon} from 'react-native-elements';
+
+const RouterComponent = () => {
+  return (
+    <Router>
+      <Stack key="root" headerLayoutPreset="center">
+        <Scene key="login" hideNavBar={true} component={LoginPage} />
+        <Scene key="signup"
+          component={SignupPage}
+          navigationBarStyle={style.nav}
+          title="Register"
+          titleStyle={style.title} 
+          renderLeftButton={() => renderBackButton()}   
+          />
+      </Stack>
+    </Router>
+  );
+};
+
+const renderBackButton = () => (
+  <TouchableOpacity
+      onPress={() => Actions.pop()}
+  >
+      <View style={{ alignItems: 'center' }}>
+        <Icon  iconStyle={style.iconStyle} name="keyboard-arrow-left" type="MaterialIcons" />
+      </View>
+  </TouchableOpacity>
+);
+
+
+const style = StyleSheet.create({
+  nav: {
+    backgroundColor: '#016C9A',
+    textAlign: 'center',
+  },
+
+  title: {
+    color: 'white',
+    flex: 1,
+  },
+  iconStyle: {
+    color: 'white',
+  }
+})
+
+
+
+export default RouterComponent;
