b40989c188b199417e8a59225e3a346d62d35af4
Evelyn Zhang <ruz164@ucsd.edu>
2019-11-22 17:41:53 -0800

    add list to profile page

--- TritonDeal/RouterComponent.js
+++ TritonDeal/RouterComponent.js
@@ -3,35 +3,39 @@ import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
 import {Scene, Router, Stack, Actions} from 'react-native-router-flux';
 import LoginPage from './src/views/LoginPage';
 import SignupPage from './src/views/SignupPage';
+import Profile from './src/views/Profile';
 import {Icon} from 'react-native-elements';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Stack key="root" headerLayoutPreset="center">
-        <Scene key="login" hideNavBar={true} component={LoginPage} />
-        <Scene key="signup"
+        <Scene key="profile" hideNavBar={true} component={Profile} />
+        <Scene
+          key="signup"
           component={SignupPage}
           navigationBarStyle={style.nav}
           title="Register"
-          titleStyle={style.title} 
-          renderLeftButton={() => renderBackButton()}   
-          />
+          titleStyle={style.title}
+          renderLeftButton={() => renderBackButton()}
+        />
+        <Scene key="login" hideNavBar={true} component={LoginPage} />
       </Stack>
     </Router>
   );
 };
 
 const renderBackButton = () => (
-  <TouchableOpacity
-      onPress={() => Actions.pop()}
-  >
-      <View style={{ alignItems: 'center' }}>
-        <Icon  iconStyle={style.iconStyle} name="keyboard-arrow-left" type="MaterialIcons" />
-      </View>
+  <TouchableOpacity onPress={() => Actions.pop()}>
+    <View style={{alignItems: 'center'}}>
+      <Icon
+        iconStyle={style.iconStyle}
+        name="keyboard-arrow-left"
+        type="MaterialIcons"
+      />
+    </View>
   </TouchableOpacity>
 );
-
 
 const style = StyleSheet.create({
   nav: {
@@ -45,9 +49,7 @@ const style = StyleSheet.create({
   },
   iconStyle: {
     color: 'white',
-  }
-})
-
-
+  },
+});
 
 export default RouterComponent;
--- TritonDeal/src/views/Profile.js
+++ TritonDeal/src/views/Profile.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
-import {ListItem, Icon, Button} from 'react-native-elements';
+import {Icon, Button} from 'react-native-elements';
+import {InfoList, ChoiceList} from '../components/ProfileList';
 
 export default class UserProfileView extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class UserProfileView extends Component {
       <View style={styles.container}>
         <View style={styles.header}>
           <View style={styles.headerContent}>
+            <Text style={styles.headerText}>My Profile</Text>
             <Image
               style={styles.avatar}
               source={{
@@ -15,117 +17,65 @@ export default class UserProfileView extends Component {
                   'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-9/54519275_630941387367096_5376254316682149888_o.jpg?_nc_cat=105&_nc_oc=AQka3xZuK6vCwU2fyykoP52RbX3tWm-NOcZuB-hPyvfDylY-HcML6yZDQjMxybkFDv4&_nc_ht=scontent-lax3-1.xx&oh=fd4f9134ee28cfa4ad557abd1e7e8897&oe=5E3F0B06',
               }}
             />
-            <Text style={styles.name}>Michael Yao </Text>
           </View>
         </View>
-        <Button title="Edit Profile" />
-        <List />
-
         <Button
-          icon={<Icon name="shopping-cart" size={20} color="white" />}
-          title="My Cart"
+          icon={<Icon name="edit" type="font-awesome" color="#005688" />}
+          iconLeft
+          titleStyle={editButtonStyle.title}
+          buttonStyle={editButtonStyle.button}
+          title="Edit Profile"
         />
-        <Button
-          icon={<Icon name="inbox" size={20} color="white" />}
-          title="My Post"
-        />
-        <Button
-          icon={<Icon name="list" size={20} color="white" />}
-          title="My Purchase"
-        />
+        <View style={styles.background}>
+          <InfoList />
+          <ChoiceList />
+        </View>
       </View>
     );
   }
 }
 
-const List = () => {
-  const list = [
-    // {
-    //     title: 'Michael yao',
-    //     icon: 'verified-user'
-    // },
-    {
-      title: 'michael@gmail.com',
-      icon: 'email',
-    },
-    {
-      title: '123455677',
-      icon: 'phone',
-    },
-    {
-      title: '1234 Caminito Sueno',
-      icon: 'navigation',
-    },
-  ];
-  return (
-    <View style={styles.list}>
-      {list.map((item, i) => (
-        <ListItem
-          key={i}
-          title={item.title}
-          leftIcon={{name: item.icon}}
-          bottomDivider
-        />
-      ))}
-    </View>
-  );
-};
+const editButtonStyle = StyleSheet.create({
+  title: {
+    color: '#005688',
+  },
+  button: {
+    width: 200,
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: -20,
+    zIndex: 1,
+    borderRadius: 10,
+  },
+});
 
 const styles = StyleSheet.create({
   header: {
-    backgroundColor: 'grey',
+    backgroundColor: '#00395B',
   },
+
+  background: {
+    paddingTop: 28,
+    backgroundColor: '#ededed',
+  },
+
   headerContent: {
-    padding: 30,
+    padding: 17,
     alignItems: 'center',
   },
   avatar: {
     width: 130,
-    height: 130,
+    height: 125,
     borderRadius: 63,
     borderWidth: 4,
     borderColor: 'white',
     marginBottom: 10,
   },
-  name: {
-    fontSize: 22,
-    color: '#000000',
-    fontWeight: '600',
-  },
-  userInfo: {
-    fontSize: 16,
-    color: '#778899',
-    fontWeight: '600',
-  },
-  body: {
-    backgroundColor: '#778899',
-    height: 500,
-    alignItems: 'center',
-  },
-  item: {
-    flexDirection: 'row',
-  },
-  infoContent: {
-    flex: 1,
-    alignItems: 'flex-start',
-    paddingLeft: 5,
-  },
-  iconContent: {
-    flex: 1,
-    alignItems: 'flex-end',
-    paddingRight: 5,
-  },
-  icon: {
-    width: 30,
-    height: 30,
-    marginTop: 20,
-  },
-  info: {
-    fontSize: 18,
-    marginTop: 20,
-    color: '#FFFFFF',
-  },
-  list: {
-    marginBottom: 50,
+
+  headerText: {
+    fontSize: 20,
+    color: 'white',
+    marginBottom: 5,
   },
 });
