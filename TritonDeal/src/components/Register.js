3c834d2adaa06e749199e1c218005cc2b1ec9705
Evelyn Zhang <ruz164@ucsd.edu>
2019-11-23 18:22:40 -0800

    add message handle to login and register page

--- TritonDeal/src/components/Register.js
+++ TritonDeal/src/components/Register.js
@@ -6,13 +6,18 @@ import auth from '@react-native-firebase/auth';
 import { firebase } from '@react-native-firebase/auth';
 import { Actions } from 'react-native-router-flux';
 import Dialog from './Dialog';
+import message from '../message';
 
 export default class Register extends React.Component {
 
   state = {
     email: 'example@ucsd.edu',
     password: 'password',
-    unsubscribe: null
+    unsubscribe: null,
+    isVisible: false,
+    dialogText: '',
+    passwordError: '',
+    emailError: '',
   };
 
   componentDidMount = () => {
@@ -30,14 +35,11 @@ export default class Register extends React.Component {
             dynamicLinkDomain: "tritondeal.page.link"
           };
           user.sendEmailVerification(actionCodeSettings).then(() => {
-            Alert.alert(
-              null,
-              'Verification link has been sent to your email address.',
-              [
-                {text: 'OK', onPress: () => {Actions.pop()}}
-              ],
-              {cancelable: false}
-            );
+            this.setState(prev => ({
+              ...prev,
+              isVisible: true,
+              dialogText: message.VALIDATE_EAMIL,
+            }));
           });
         }
       }
@@ -53,23 +55,41 @@ export default class Register extends React.Component {
   }
 
   handleCreateUser = () => {
-    return (
-      firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        if (errorCode) {
-          if (errorCode == 'auth/weak-password') {
-              this.setState({ isVisible: true, text:"Your password is too weak."});
-          } else if (errorCode == 'auth/invalid-email') {
-            ToastAndroid.show('The email address is invalid.', ToastAndroid.SHORT);
-          } else if (errorCode == 'auth/email-already-in-use') {
-            ToastAndroid.show('The email address is already in use.', ToastAndroid.SHORT);
-          } else {
-            ToastAndroid.show(errorMessage, ToastAndroid.SHORT);
-          }
+    this.setState(prev => ({...prev,
+      dialogText: '',
+      passwordError: '',
+      emailError: '',}));
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode) {
+        if (errorCode == 'auth/weak-password') {
+          this.setState(prev => ({
+            ...prev,
+            emailError: '',
+            passwordError: message.WEAK_PASSWORD,
+          }));
+        } else if (errorCode == 'auth/invalid-email') {
+          this.setState(prev => ({
+            ...prev,
+            passwordError: '',
+            emailError: message.INVALID_EMAIL,
+          }));
+        } else if (errorCode == 'auth/email-already-in-use') {
+          this.setState(prev => ({
+            ...prev,
+            passwordError: '',
+            emailError: message.EMAIL_IN_USE,
+          }));
+        } else {
+          this.setState(prev => ({
+            ...prev,
+            isVisible: true,
+            dialogText: errorMessage,
+          }));
         }
-      })
-    );
+      }
+    })
   };
 
   render() {
@@ -82,6 +102,8 @@ export default class Register extends React.Component {
           }
           keyboardType="email-address"
           onChangeText={(value) => this.setState({ email: value })}
+          errorStyle={style.errorStyle}
+          errorMessage={this.state.emailError}
         />
         <Input
           placeholder="Enter your username"
@@ -96,6 +118,8 @@ export default class Register extends React.Component {
           }
           secureTextEntry={true}
           onChangeText={(value) => this.setState({ password: value })}
+          errorStyle={style.errorStyle}
+          errorMessage={this.state.passwordError}
         />
         <Input
           placeholder="Confirm Password"
@@ -115,7 +139,7 @@ export default class Register extends React.Component {
           text={"Sign Up"}
           onPress={this.handleCreateUser}
         />
-        <Dialog isVisible={this.state.isVisible} text={this.state.text}/>
+        <Dialog isVisible={this.state.isVisible} text={this.state.dialogText} onPress={() => {this.setState({isVisible: false}); Actions.pop();}} />
       </View>
     )
   };
@@ -144,6 +168,13 @@ const style = StyleSheet.create({
   container: {
     paddingHorizontal: 38,
     textAlign: 'center',
+  },
+
+  errorStyle: {
+    color: 'red',
+    paddingLeft: 45,
+    fontSize: 18,
   }
+
 });;
 
