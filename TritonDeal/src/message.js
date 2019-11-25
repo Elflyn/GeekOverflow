0cb3711cec2362afb34172ece3bdfe596efb2291
Zhaoqian Wang <zhw327@ucsd.edu>
2019-11-25 01:04:38 -0800

    Optimized login and register logic.

--- TritonDeal/src/message.js
+++ TritonDeal/src/message.js
@@ -2,7 +2,7 @@ export default {
   WEAK_PASSWORD: 'This password is too weak.',
   INVALID_EMAIL: 'This email is invalid',
   EMAIL_IN_USE: 'This email is already in use',
-  VALIDATE_EAMIL: 'Greate! An email is sent to your email address to verify. Please click the link inside the email to activeate your account.',
+  VALIDATE_EAMIL: 'Great! An email is sent to your email address to verify. Please click the link inside the email to activeate your account.',
   RESET_PASSWORD: 'An email is sent to your email address to reset password.',
   ACCOUNT_DISABLED: 'Sorry! Your account is disabled.',
   WORNG_COMBINATION: 'Wrong email/password combination.',
