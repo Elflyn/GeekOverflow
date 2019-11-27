3ed863eb94081b65be2e1d0922b866642f5597c2
michael <juy075@ucsd.edu>
2019-11-26 17:39:04 -0800

    bottom bar

--- TritonDeal/RouterComponent.js
+++ TritonDeal/RouterComponent.js
@@ -9,17 +9,42 @@ import Dialog from './src/components/Overlay';
 import EditProfile from './src/views/EditProfile';
 
 const RouterComponent = () => {
+  const TabIcon = ({ selected, title }) => {
+    return (
+        <Text style={{color: selected ? 'red' :'black'}}>{title}</Text>
+    );
+  }
+
   return(
     <Router>
       <Stack key="root" headerLayoutPreset="center">
+        <Scene
+            key="tabbar"
+            tabs={true}
+            tabBarStyle={{ backgroundColor: '#FFFFFF' }}
+            hideNavBar={true}
+        >
+          <Scene key="osu" title="HOME" icon={TabIcon}>
+            <Scene
+                key="sigup"
+                component={SignupPage}
+                navigationBarStyle={style.nav}
+                title="Register"
+                titleStyle={style.title}
+            />
+          </Scene>
+          <Scene key="um" title="Profile" icon={TabIcon}>
+            <Scene
+                key="editProfile"
+                component={EditProfile}
+                navigationBarStyle={style.nav}
+                title="Edit Profile"
+                titleStyle={style.title}
+            />
+          </Scene>
+        </Scene>
+
         <Scene key="login" hideNavBar={true} component={LoginPage} />
-        <Scene key="EditProfile"
-               component={EditProfile}
-               navigationBarStyle={style.nav}
-               title="Edit Profile"
-               titleStyle={style.title}
-               renderLeftButton={() => renderBackButton()}
-        />
         <Scene key="signup"
           component={SignupPage}
           navigationBarStyle={style.nav}
@@ -27,7 +52,6 @@ const RouterComponent = () => {
           titleStyle={style.title}
           renderLeftButton={() => renderBackButton()}
         />
-        <Scene key="login" hideNavBar={true} component={LoginPage} />
       </Stack>
     </Router>
   );
