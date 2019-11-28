b879d5e8a744f0d420cbc5c2d9055e65778922fe
Evelyn Zhang <ruz164@ucsd.edu>
2019-11-28 03:41:46 -0800

    add a chat list view

---
+++ TritonDeal/src/views/ChatList.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, View, Text, Linking, TouchableOpacity, ScrollView } from 'react-native';
+import { ListItem, Avatar} from 'react-native-elements';
+
+export default class ChatList extends React.Component {
+
+  list = [{
+    name: 'Hello',
+    message: 'super long message ffffffffffffffffffffffffffffffffffff',
+    description: 'fadfddfs',
+  },
+  {
+    name: 'World',
+    message: 'fadfasd',
+    description: 'super long description gggggggggggggggggggggggggggggggggg',
+  }]
+  render() {
+    return (
+      <ScrollView>
+      {
+        this.list.map((item, i) => (
+          <ListItem
+            key={i}
+             /* avatar : source: { uri: l.avatar_url }*/ 
+            leftAvatar={{ size: 'large', icon: {name: 'user', type: 'font-awesome'} }}
+            title={item.name}
+            titleStyle={style.title}
+            subtitle={<Subtitle message={item.message} description={item.description}/>}
+            rightElement={<TimeDisplay time="11:30"/>}
+          />))
+      }
+
+      </ScrollView>
+    )
+  }
+}
+
+const TimeDisplay = ({time}) => {
+  return (
+    <Text style={{color: 'grey'}}>{time}</Text>
+  )
+}
+
+const Subtitle = ({message, description}) => (
+  <View>
+    <Text style={style.message} numberOfLines={2}>{message}</Text>
+    <Text style={style.description} numberOfLines={1}>{description}</Text>
+  </View>
+)
+
+const style = StyleSheet.create({
+  message: {
+    fontSize: 16,
+  },
+
+  description: {
+    fontSize: 12,
+    marginTop: 5,
+  },
+
+  title: {
+    fontSize: 21,
+  }
+})
