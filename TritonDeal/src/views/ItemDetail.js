af0c31fe5d81a35c4af58445357c43315457b7e2
Evelyn Zhang <ruz164@ucsd.edu>
2019-12-08 04:32:38 -0800

    fix shopping cart and mypost

--- TritonDeal/src/views/ItemDetail.js
+++ TritonDeal/src/views/ItemDetail.js
@@ -1,79 +1,102 @@
-import React, { Component } from 'react';
-import { Button, Overlay } from 'react-native-elements';
-import { StyleSheet, View, Image, Text, ScrollView, Alert, ToastAndroid, ActivityIndicator } from 'react-native';
+import React, {Component} from 'react';
+import {Button, Overlay} from 'react-native-elements';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  ScrollView,
+  Alert,
+  ToastAndroid,
+  ActivityIndicator,
+} from 'react-native';
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/auth';
 import '@react-native-firebase/database';
-import { Actions } from 'react-native-router-flux';
+import {Actions} from 'react-native-router-flux';
 import ChatList from './ChatList';
 
 export default class ItemDetail extends Component {
-
   state = {
     chatID: null,
-    finished: true
+    finished: true,
   };
 
   componentWillMount = async () => {
     const chatListRef = firebase.database().ref('chat_by_id');
     var cid;
-    await chatListRef.orderByChild('postID').equalTo(this.props.postKey).once('value').then((snapshot) => {
-      snapshot.forEach(childSnapshot => {
-        if (firebase.auth().currentUser && firebase.auth().currentUser.uid == childSnapshot.val().user1) {
-          cid = childSnapshot.key;
-        }
+    await chatListRef
+      .orderByChild('postID')
+      .equalTo(this.props.postKey)
+      .once('value')
+      .then(snapshot => {
+        snapshot.forEach(childSnapshot => {
+          if (
+            firebase.auth().currentUser &&
+            firebase.auth().currentUser.uid == childSnapshot.val().user1
+          ) {
+            cid = childSnapshot.key;
+          }
+        });
       })
-    }).catch(error => {
-      ToastAndroid.show(error.message, ToastAndroid.SHORT);
-    })
-    this.setState({chatID: cid})
-  }
-  
+      .catch(error => {
+        ToastAndroid.show(error.message, ToastAndroid.SHORT);
+      });
+    this.setState({chatID: cid});
+  };
+
   toggleActivityIndicator = () => {
-    this.setState({ finished: !this.state.finished });
+    this.setState({finished: !this.state.finished});
   };
 
   render() {
-    const {itemName, imageSource, tags, description, price, inCart, sellerUID, username, postKey, active, cb} = this.props
+    const {
+      itemName,
+      imageSource,
+      tags,
+      description,
+      price,
+      inCart,
+      sellerUID,
+      username,
+      postKey,
+      active,
+      cb,
+    } = this.props;
     return (
       <View>
-        <Overlay
-          isVisible={!this.state.finished}
-          width="auto"
-          height="auto"
-        >
-          <ActivityIndicator size='large' color='#eabb33' />
+        <Overlay isVisible={!this.state.finished} width="auto" height="auto">
+          <ActivityIndicator size="large" color="#eabb33" />
         </Overlay>
         <View>
           <View style={style.detailTitleRow}>
             <Text style={style.detailTitle}>{itemName}</Text>
-            <View style={style.detailTopIcon}>
-          </View>
+            <View style={style.detailTopIcon} />
           </View>
           <ScrollView horizontal={true} style={style.imageScroll}>
-            {imageSource.map(image =><Image source={{uri:image}} style={style.multiImage}/>)}
+            {imageSource.map(image => (
+              <Image source={{uri: image}} style={style.multiImage} />
+            ))}
           </ScrollView>
-          <View style={{flexDirection:"row"}}>
-            <Text style={{fontSize:30,marginTop:5}}>${price}</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize: 30, marginTop: 5}}>${price}</Text>
           </View>
-          <ScrollView style={{height:160,marginTop:10}}>
+          <ScrollView style={{height: 160, marginTop: 10}}>
             <View style={style.detailTagsView}>
-              {
-                tags.map((tag,i)=>{
-                  return(
-                    <View style={style.detailTag} key={i}>
+              {tags.map((tag, i) => {
+                return  (
+                  <View style={style.detailTag} key={i}>
                     <Text style={style.detailTagText}>{tag}</Text>
-                    </View>
-                  );
-                })
-              }
+                  </View>
+                );
+              })}
             </View>
-            <Text style={{fontSize:18,padding:5}}>{description}</Text>
+            <Text style={{fontSize: 18, padding: 5}}>{description}</Text>
           </ScrollView>
-          <ActionButtons 
+          <ActionButtons
             username={username}
-            sellerUID={sellerUID}          
-            cid={this.state.chatID} 
+            sellerUID={sellerUID}
+            cid={this.state.chatID}
             imageSource={imageSource[0]}
             itemName={itemName}
             price={price}
@@ -85,214 +108,260 @@ export default class ItemDetail extends Component {
           />
         </View>
       </View>
-    )
-  }
-}
-
-const ActionButtons = ({ username, cid, sellerUID, price, itemName, imageSource, postKey, inCart, active, cb, toogle }) => {
-  if (firebase.auth().currentUser && sellerUID != firebase.auth().currentUser.uid) {
-    return (
-      <View style={{ flexDirection: "row", justifyContent: 'center' }}>
-        {active &&
-          <Button title="Contact Seller" buttonStyle={{ margin: 10 }} onPress={() => {
-            var chatID;
-            if (!cid) {
-              const cl = new ChatList();
-              chatID = cl.createChat(firebase.auth().currentUser.uid, sellerUID, imageSource, itemName, price, postKey);
-            } else {
-              chatID = cid;
-            }
-            Actions._chatList();
-            const chatInfo = { title: username, 
-              chatID: chatID, 
-              imgURI: imageSource, 
-              itemName: itemName, 
-              price: price, 
-              sellerUID: sellerUID, 
-              postID: postKey, 
-              active: true 
-            };
-          Actions.chat(chatInfo);
-          }} />
-        }
-        {inCart ?
-          <Button
-            onPress={() => {
-              toogle();
-              firebase.database().ref('user_to_cart/' + firebase.auth().currentUser.uid).child(postKey).remove()
-              cb().then(() => {
-                toogle()
-                Actions.pop();
-              });
-            }}
-            title="Remove from the Cart"
-            buttonStyle={{ margin: 10 }}
-          />
-          : <Button
-            onPress={() => {
-              const userCartRef = firebase.database().ref('user_to_cart/' + firebase.auth().currentUser.uid)
-              userCartRef.orderByChild('post').equalTo(postKey).once('value', snapshot => {
-                if (snapshot.exists()) {
-                  Alert.alert('', 'This item is already in your cart! Check out your shopping Cart!', [{ text: 'OK', onDismiss: () => { } }])
-                } else {
-                  userCartRef.push({ post: postKey })
-                  Alert.alert('', 'You have successfully added this item to you chart!', [{ text: 'OK', onDismiss: () => { } }])
-                }
-              })
-            }}
-            title="Add to Cart"
-            buttonStyle={{ margin: 10 }}
-          />
-        }
-      </View>
-    )
-  } else if (firebase.auth().currentUser && active) {
-    return (
-      <View style={{ flexDirection: "row", justifyContent: 'center' }}>
-        <Button title="Cancel this listing" buttonStyle={{ margin: 10 }} onPress={() => {
-          Alert.alert(
-            null,
-            'This item cannot be viewed by other users once you cancel this listing.',
-            [
-              {
-                text: 'Cancel',
-                style: 'cancel',
-              },
-              {
-                text: 'OK', onPress: async () => {
-                  const postRef = firebase.database().ref('post');
-                  postRef.child(postKey).update({ active: false });
-                  Actions.pop();
-                }
-              },
-            ],
-          );
-        }} />
-      </View>
-    )
-  } else {
-    return (
-      <Text style={style.text}>Please login to view more</Text>
     );
   }
 }
 
+const ActionButtons = ({
+  username,
+  cid,
+  sellerUID,
+  price,
+  itemName,
+  imageSource,
+  postKey,
+  inCart,
+  active,
+  cb,
+  toogle,
+}) => {
+  if (
+    firebase.auth().currentUser &&
+    sellerUID != firebase.auth().currentUser.uid
+  ) {
+    return (
+      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        {active && (
+          <Button
+            title="Contact Seller"
+            buttonStyle={{margin: 10}}
+            onPress={() => {
+              var chatID;
+              if (!cid) {
+                const cl = new ChatList();
+                chatID = cl.createChat(
+                  firebase.auth().currentUser.uid,
+                  sellerUID,
+                  imageSource,
+                  itemName,
+                  price,
+                  postKey,
+                );
+              } else {
+                chatID = cid;
+              }
+              Actions._chatList();
+              const chatInfo = {
+                title: username,
+                chatID: chatID,
+                imgURI: imageSource,
+                itemName: itemName,
+                price: price,
+                sellerUID: sellerUID,
+                postID: postKey,
+                active: true,
+              };
+              Actions.chat(chatInfo);
+            }}
+          />
+        )}
+        {inCart ? (
+          <Button
+            onPress={() => {
+              toogle();
+              firebase
+                .database()
+                .ref('user_to_cart/' + firebase.auth().currentUser.uid)
+                .child(postKey)
+                .remove();
+              cb().then(() => {
+                toogle();
+                Actions.pop();
+              });
+            }}
+            title="Remove from the Cart"
+            buttonStyle={{margin: 10}}
+          />
+        ) : (
+          <Button
+            onPress={() => {
+              const userCartRef = firebase
+                .database()
+                .ref('user_to_cart/' + firebase.auth().currentUser.uid);
+              userCartRef
+                .orderByChild('post')
+                .equalTo(postKey)
+                .once('value', snapshot => {
+                  if (snapshot.exists()) {
+                    Alert.alert(
+                      '',
+                      'This item is already in your cart! Check out your shopping Cart!',
+                      [{text: 'OK', onDismiss: () => {}}],
+                    );
+                  } else {
+                    userCartRef.push({post: postKey});
+                    Alert.alert(
+                      '',
+                      'You have successfully added this item to you chart!',
+                      [{text: 'OK', onDismiss: () => {}}],
+                    );
+                  }
+                });
+            }}
+            title="Add to Cart"
+            buttonStyle={{margin: 10}}
+          />
+        )}
+      </View>
+    );
+  } else if (firebase.auth().currentUser && active) {
+    return (
+      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        <Button
+          title="Cancel this listing"
+          buttonStyle={{margin: 10}}
+          onPress={() => {
+            Alert.alert(
+              null,
+              'This item cannot be viewed by other users once you cancel this listing.',
+              [
+                {
+                  text: 'Cancel',
+                  style: 'cancel',
+                },
+                {
+                  text: 'OK',
+                  onPress: async () => {
+                    const postRef = firebase.database().ref('post');
+                    postRef.child(postKey).update({active: false});
+                    Actions.pop();
+                  },
+                },
+              ],
+            );
+          }}
+        />
+      </View>
+    );
+  } else {
+    return <Text style={style.text}>Please login to view more</Text>;
+  }
+};
+
 const style = StyleSheet.create({
-  container:{
+  container: {
     flexDirection: 'row',
-    borderWidth:1,
-    borderColor:"white",
-    borderRadius:1,
-    elevation:1,
-    margin:5,
-    padding:2,
-    justifyContent:"center"
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 1,
+    elevation: 1,
+    margin: 5,
+    padding: 2,
+    justifyContent: 'center',
   },
-  textView:{
-    flex:2,
-    flexDirection:'column',
-    height:130,
+  textView: {
+    flex: 2,
+    flexDirection: 'column',
+    height: 130,
     width: 260,
-    marginTop:5
+    marginTop: 5,
   },
   image: {
-    flex:1,
-    height: "auto",
-    width: "auto",
-    borderRadius:10,
-    margin:5,
+    flex: 1,
+    height: 'auto',
+    width: 'auto',
+    borderRadius: 10,
+    margin: 5,
   },
-  itemTitle:{
-    fontSize:20,
-    color:"black",
+  itemTitle: {
+    fontSize: 20,
+    color: 'black',
   },
-  tagsView:{
+  tagsView: {
     //flex:1,
-    padding:1,
-    flexDirection:"row",
-    flexWrap:"wrap",
+    padding: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     //justifyContent:"space-between"
   },
-  tag:{
-    padding:3,
-    marginRight:10,
-    borderRadius:5,
-    backgroundColor:"#B6B1A9",
+  tag: {
+    padding: 3,
+    marginRight: 10,
+    borderRadius: 5,
+    backgroundColor: '#B6B1A9',
   },
-  tagText:{
-    color:"black",
+  tagText: {
+    color: 'black',
   },
-  price:{
-    flex:1,
-    fontSize:24,
-
+  price: {
+    flex: 1,
+    fontSize: 24,
   },
-  bottomStyle:{
-    flexDirection:"row",
-    justifyContent:"space-between"
+  bottomStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
-  rightView:{
-    flex:1,
-    flexDirection:"row-reverse",
-    justifyContent:"space-between",
+  rightView: {
+    flex: 1,
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
   },
-  button:{
-    marginLeft:40,
-    marginBottom:4,
-    height:37,
-    width:50,
-    borderRadius:5,
+  button: {
+    marginLeft: 40,
+    marginBottom: 4,
+    height: 37,
+    width: 50,
+    borderRadius: 5,
   },
-  detailTitleRow:{
-    flexDirection:"row",
+  detailTitleRow: {
+    flexDirection: 'row',
   },
-  detailTitle:{
-    flex:1,
-    fontSize:30,
-    margin:5
+  detailTitle: {
+    flex: 1,
+    fontSize: 30,
+    margin: 5,
   },
-  detailTopIcon:{
-    flex:0.2,
-    flexDirection:"row",
-    justifyContent:"flex-end",
-    marginTop:12,
+  detailTopIcon: {
+    flex: 0.2,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 12,
   },
-  imageScroll:{
-    borderBottomWidth:1,
-    borderColor:"#747678",
-    padding:2,
+  imageScroll: {
+    borderBottomWidth: 1,
+    borderColor: '#747678',
+    padding: 2,
     height: 280,
   },
-  multiImage:{
-    width:370,
-    height:270,
-    margin:10,
-    alignSelf:"center"
+  multiImage: {
+    width: 370,
+    height: 270,
+    margin: 10,
+    alignSelf: 'center',
   },
-  detailTagsView:{
+  detailTagsView: {
     //flex:1,
-    paddingLeft:5,
-    paddingRight:5,
-    flexDirection:"row",
-    flexWrap:"wrap",
+    paddingLeft: 5,
+    paddingRight: 5,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     //justifyContent:"space-between"
   },
-  detailTag:{
-    padding:1,
-    marginTop:5,
-    marginRight:10,
-    borderRadius:5,
-    backgroundColor:"#B6B1A9",
+  detailTag: {
+    padding: 1,
+    marginTop: 5,
+    marginRight: 10,
+    borderRadius: 5,
+    backgroundColor: '#B6B1A9',
   },
-  detailTagText:{
-    color:"black",
-    fontSize:22
+  detailTagText: {
+    color: 'black',
+    fontSize: 22,
   },
   text: {
     textAlign: 'center',
     color: 'gray',
     fontSize: 18,
-    marginTop: 50
+    marginTop: 50,
   },
-
 });
--- TritonDeal/src/views/ShoppingCart.js
+++ TritonDeal/src/views/ShoppingCart.js
@@ -1,103 +1,135 @@
 import React from 'react';
-import { ScrollView, Text, StyleSheet, ActivityIndicator, View} from 'react-native';
-import ItemDisplay from '../components/ItemDisplay'
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  ActivityIndicator,
+  View,
+} from 'react-native';
+import ItemDisplay from '../components/ItemDisplay';
 import firebase from '@react-native-firebase/app';
 import '@react-native-firebase/database';
 import '@react-native-firebase/auth';
 import '@react-native-firebase/storage';
 
 export default class ShoppingCart extends React.Component {
-
   state = {
     post: [],
     inactive: [],
-    finished: false,
+    finished: true,
     empty: false,
-  }
-
-  toggleActivityIndicator = () => {
-    this.setState({ finished: !this.state.finished });
   };
 
-  onResolve = (url) => {
+  toggleActivityIndicator = () => {
+    this.setState({finished: !this.state.finished});
+  };
+
+  onResolve = url => {
     return url;
-  }
+  };
 
   onReject = async err => {
-    return await firebase.storage().ref('avatar').child('default.jpg').getDownloadURL();
-  }
-  
+    return await firebase
+      .storage()
+      .ref('avatar')
+      .child('default.jpg')
+      .getDownloadURL();
+  };
+
   getShoppingCart = async () => {
-    this.toggleActivityIndicator()
-    let arr = []
-    let inactive = []
-    await firebase.database().ref('user_to_cart/' + firebase.auth().currentUser.uid).once('value').then(async data => {
-      const d = JSON.parse(JSON.stringify(data))
-      for (var key in d) {
-        await firebase.database().ref('post/' + d[key].post).once('value').then(async post => {
-          const p = JSON.parse(JSON.stringify(post))
-          const avatar = await firebase.storage().ref('avatar').child(p.user).getDownloadURL().then(this.onResolve, this.onReject);
-          let i = 0
-          let photos = [];
-          for(; i < p.photos; i++) {
-            const photo = await firebase.storage().ref('postImage').child(`${d[key].post}-${i}`).getDownloadURL();
-            photos.push(photo);
-          }
-          if (p.active === true) {
-            arr.push({title: p.title, source: photos, tags: p.tags, description: p.description, price: p.price, seller: avatar, sellerUID: p.user, username: p.username, key: key})
-          } else {
-            inactive.push({title: p.title, source: photos, tags: p.tags, description: p.description, price: p.price, seller: avatar, sellerUID: p.user, username: p.username, key: key})
-          }
-        })
-      }
-    })
-    this.setState({post: arr, empty: arr.length === 0, inactive: inactive})
-    this.toggleActivityIndicator()
-  }
+    this.toggleActivityIndicator();
+
+    let arr = [];
+
+    let inactive = [];
+
+    await firebase
+      .database()
+      .ref('user_to_cart/' + firebase.auth().currentUser.uid)
+      .once('value')
+      .then(async data => {
+        const d = JSON.parse(JSON.stringify(data));
+
+        for (var key in d) {
+          await firebase
+            .database()
+            .ref('post/' + d[key].post)
+            .once('value')
+            .then(async post => {
+              const p = JSON.parse(JSON.stringify(post));
+
+              const avatar = await firebase
+                .storage()
+                .ref('avatar')
+                .child(p.user)
+                .getDownloadURL()
+                .then(this.onResolve, this.onReject);
+              let i = 0;
+
+              let photos = [];
+              for (; i < p.photos; i++) {
+                const photo = await firebase
+                  .storage()
+                  .ref('postImage')
+                  .child(`${d[key].post}-${i}`)
+                  .getDownloadURL();
+                photos.push(photo);
+              }
+              if (p.active === true) {
+                arr.push({
+                  title: p.title,
+                  source: photos,
+                  tags: p.tags,
+                  description: p.description,
+                  price: p.price,
+                  seller: avatar,
+                  sellerUID: p.user,
+                  username: p.username,
+                  key: key,
+                });
+              } else {
+                inactive.push({
+                  title: p.title,
+                  source: photos,
+                  tags: p.tags,
+                  description: p.description,
+                  price: p.price,
+                  seller: avatar,
+                  sellerUID: p.user,
+                  username: p.username,
+                  key: key,
+                });
+              }
+            });
+        }
+      });
+
+    this.setState({
+      post: arr,
+      empty: arr.length === 0 && inactive.length === 0,
+      inactive: inactive,
+    });
+    this.toggleActivityIndicator();
+  };
 
   componentDidMount = () => {
-    this.getShoppingCart()
-  }
-  
+    this.getShoppingCart();
+  };
+
   render() {
+    console.log(this.state);
     return (
       <ScrollView style={style.container}>
-        {this.state.finished && <ActivityIndicator size='large' color='#eabb33' />}
-        { this.state.empty ? 
-          <Text style={style.text}>Your cart is empty.</Text> :
-          !this.state.finished && (
-          <View>
-            {
-              this.state.post.length !== 0 && 
-              <View>
-                <Text style={style.title}>Available</Text>
-                {
-                  this.state.post.map((item, index) => 
-                    <ItemDisplay 
-                      itemName={item.title}
-                      imageSource={item.source}
-                      tags={item.tags}
-                      description={item.description}
-                      price={item.price}
-                      seller={item.seller}
-                      postKey={item.key}
-                      sellerUID={item.sellerUID}
-                      username={item.username}
-                      inCart={true}
-                      active={true}
-                      cb={this.getShoppingCart}
-                    />)
-
-                }
-              </View>  
-            }
-            {
-              this.state.inactive.length !== 0 &&
-              <View>
-                <Text style={style.title}>InActive: </Text>
-                {
-                  this.state.inactive.map((item, index) => 
-                    <ItemDisplay 
+        {this.state.finished ? (
+          this.state.empty ? (
+            <Text style={style.text}>Your cart is empty</Text>
+          ) : (
+            <View>
+              {this.state.post.length !== 0 && (
+                <View>
+                  <Text style={style.title}>Available: </Text>
+                  {this.state.post.map((item, index) => (
+                    <ItemDisplay
                       itemName={item.title}
                       imageSource={item.source}
                       tags={item.tags}
@@ -111,35 +143,55 @@ export default class ShoppingCart extends React.Component {
                       active={false}
                       cb={this.getShoppingCart}
                     />
-                  )}
-              </View>
-
-            }
-
-          </View>)
-          
-        }
+                  ))}
+                </View>
+              )}
+              {this.state.inactive.length !== 0 && (
+                <View>
+                  <Text style={style.title}>InActive: </Text>
+                  {this.state.inactive.map((item, index) => (
+                    <ItemDisplay
+                      itemName={item.title}
+                      imageSource={item.source}
+                      tags={item.tags}
+                      description={item.description}
+                      price={item.price}
+                      seller={item.seller}
+                      postKey={item.key}
+                      sellerUID={item.sellerUID}
+                      username={item.username}
+                      inCart={true}
+                      active={false}
+                      cb={this.getShoppingCart}
+                    />
+                  ))}
+                </View>
+              )}
+            </View>
+          )
+        ) : (
+          <ActivityIndicator size="large" color="#eabb33" />
+        )}
       </ScrollView>
-    )
+    );
   }
 }
 
 const style = StyleSheet.create({
-  text:{
+  text: {
     textAlign: 'center',
     color: 'gray',
     fontSize: 18,
-    marginTop: 50
+    marginTop: 50,
   },
 
   container: {
     marginVertical: 10,
   },
 
-  title:{
+  title: {
     marginTop: 10,
     fontSize: 20,
     marginLeft: 10,
-
-  }
-})
+  },
+});
